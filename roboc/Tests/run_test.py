4812350000a9ce681557022845cdbf382c59c903
Antoine Rouquette <arouquette@dxo.com>
2017-03-08 16:16:09 +0100

    Add run_test for unit test and coverage

---
+++ roboc/Tests/run_test.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+import glob
+
+
+def print_and_call(cmd):
+    print("> {}".format(" ".join(cmd)))
+    sys.stdout.flush()
+    subprocess.check_call(cmd)
+
+
+def run_utest():
+
+    cmd = ["coverage", "run", "-m", "unittest"]
+    print_and_call(cmd)
+    cmd = ["coverage", "html"]
+    print_and_call(cmd)
+
+if __name__ == '__main__':
+    run_utest()
