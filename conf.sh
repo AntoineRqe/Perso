7fdc301154e24e0b96f92de16dbca8751ecf07b7
Antoine Rouquette <antoine.rqe@gmail.com>
2018-07-04 12:29:10 +0200

    complete conf.sh

--- conf.sh
+++ conf.sh
@@ -1,4 +1,14 @@
 apt-get update
 apt-get upgrade
-apt-get install terminator geany geany-plugins git ssh
+apt-get install terminator geany geany-plugins git gitk ssh vim
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.fe fetch -a
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.unstage 'reset HEAD --'
+git config --global user.name "Antoine Rouquette"
+git config --global user.email "antoine.rqe@gmail.com"
+
 echo "antoine ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
