ea95cfff14eac2498e0ac64e30c49a533f266a9e
Antoine Rouquette <antoine.rqe@gmail.com>
2018-07-10 16:56:08 +0200

    update

--- conf.sh
+++ conf.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get upgrade
-apt-get install terminator geany geany-plugins git gitk ssh vim netcat netstat git-gui cscope meld ack-grep
+apt-get install terminator geany geany-plugins git gitk ssh vim netcat git-gui cscope meld ack-grep
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
@@ -10,6 +10,7 @@ git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'
 git config --global user.name "Antoine Rouquette"
 git config --global user.email "antoine.rqe@gmail.com"
+git config --global core.editor vim
 
 echo "antoine ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "cd /home/antoine/Perso/rootkit" >> /root/.bashrc
