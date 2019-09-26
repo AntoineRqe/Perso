d60cfb50146c28d75bf18680b745b04c6504dfe4
AntoineRqe <antoine.rqe@gmail.com>
2019-09-26 14:45:20 +0200

    Check on input

--- LCD/LCD/custom_print.h
+++ LCD/LCD/custom_print.h
@@ -5,7 +5,7 @@
 #ifndef CUSTOM_PRINT_H
 #define CUSTOM_PRINT_H
 
-#define DEBUG 0
+#define DEBUG (0)
 
 #define PRINT_INFO(fmt, ...) do{fprintf(stdout,fmt "\n", __VA_ARGS__);} while(0)
 #define PRINT_DEBUG(fmt, ...) do{if(DEBUG){fprintf(stderr, "[%s:%d] " fmt "\n", __func__, __LINE__, __VA_ARGS__);}} while(0)
--- LCD/LCD/main.c
+++ LCD/LCD/main.c
@@ -7,11 +7,11 @@
 #include "ascii.h"
 #include "matrix.h"
 
-#define	TEST_ITERATION (112)
+#define TEST (0)
+#define	TEST_ITERATION (220)
 
 int main(int argc, char* argv[])
 {
-	int					test = 0;
 	int					i = 0;
 	int					ret				= -1;
 	int					time_sec		= 0;
@@ -21,67 +21,75 @@ int main(int argc, char* argv[])
 	chained_string_t*	head = NULL;
 	matrix_t			matrix;
 
-	if (argc == 2)
-		if (!strcmp(argv[1], "test"))
-			test = 1;
+#if TEST
+	static char* text = "abcdefghijklmnopqrstuvwxyz";
+	string_to_list(text, &head);
 
-	if (test)
+	for (i = 0; i < TEST_ITERATION; i++)
 	{
-		static char* text = "hello world";
-		string_to_list(text, &head);
-
-		for (i = 0; i < TEST_ITERATION; i++)
-		{
-			build_matrix(i, head, matrix);
-			dispay_matrix(matrix);
-		}
-	}
-	else
-	{
-		PRINT_INFO("desired time on lcd? (max:9999)");
-		if (!(fgets(input_time, sizeof(input_time), stdin)))
-		{
-			PRINT_ERROR("error reading time...");
-			goto failure;
-		}
-
-		if (sscanf_s(input_time, "%d", &time_sec) == EOF)
-		{
-			strerror_s(error_buf, sizeof(error_buf), errno);
-			PRINT_ERROR("%s not a valid time : %s", input_time, error_buf);
-			goto failure;
-		}
-
-		PRINT_INFO("%s", "Desired text on LCD? (max:128 letters)");
-
-		if (!(fgets(input_text, sizeof(input_text), stdin)))
-		{
-			PRINT_ERROR("Error reading text...");
-			goto failure;
-		}
-
-		input_text[strlen(input_text) - 1] = '\0';
-
-		PRINT_DEBUG("Received \"%s\" for time %d.", input_text, time_sec);
-
-		if (string_to_list(input_text, &head) < 0)
-			goto failure;
-
-		chained_string_t* cur = head;
-
-		while (cur)
-		{
-			PRINT_DEBUG("%d letter is index %d", i, cur->index);
-			cur = cur->next;
-			i++;
-		}
-
-		if (build_matrix(time_sec, head, matrix) < 0)
-			goto failure;
-
+		build_matrix(i, head, matrix);
 		dispay_matrix(matrix);
 	}
+#else
 
+	PRINT_INFO("desired time on lcd? (max:9999)");
+	if (!(fgets(input_time, sizeof(input_time), stdin)))
+	{
+		PRINT_ERROR("error reading time...");
+		goto failure;
+	}
+
+	ret = sscanf_s(input_time, "%d", &time_sec);
+
+	if ((ret == EOF) || (ret == 0))
+	{
+		strerror_s(error_buf, sizeof(error_buf), errno);
+		PRINT_ERROR("%s not a valid time : %s", input_time, error_buf);
+		ret = -1;
+		goto failure;
+	}
+	else if (time_sec < 0)
+	{
+		PRINT_ERROR("time should be greater than 0, error with %d", time_sec);
+		ret = -1;
+		goto failure;
+	}
+
+	PRINT_INFO("%s", "Desired text on LCD? (max:128 letters)");
+
+	if (!(fgets(input_text, sizeof(input_text), stdin)))
+	{
+		PRINT_ERROR("Error reading text...");
+		goto failure;
+	}
+
+	// remove chariot return at end of string
+	input_text[strlen(input_text) - 1] = '\0';
+
+	if (input_text[0] == '\0')
+	{
+		PRINT_ERROR("text is empty...");
+		goto failure;
+	}
+
+	if (string_to_list(input_text, &head) < 0)
+		goto failure;
+
+	PRINT_DEBUG("Received \"%s\" for time %d.", input_text, time_sec);
+	chained_string_t* cur = head;
+
+	while (cur)
+	{
+		PRINT_DEBUG("%d letter is index %d", i, cur->index);
+		cur = cur->next;
+		i++;
+	}
+
+	if (build_matrix(time_sec, head, matrix) < 0)
+		goto failure;
+
+	dispay_matrix(matrix);
+#endif
 	ret = 0;
 
 failure:
